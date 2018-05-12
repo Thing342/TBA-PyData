c794af56fb3ad968aed0a0db73ab6836330913e2
Wes Jordan <wes@wesj.org>
2018-05-12 16:04:31 -0400

    add release info

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tba-pydata',
+    version='0.1',
+    packages=['tba_pydata', 'tba_pydata.games'],
+    url='https://github.com/Thing342/TBA-PyData',
+    license='MIT',
+    author='wes',
+    author_email='wes@wesj.org',
+    description='Wrapper for working with the Blue Alliance API in pandas'
+)
